c3fc1342f0d2279f1c1a3d0753a32f388c74e73a
Tingting Jiang <tvj@vt.edu>
2016-09-19 19:49:57 +0000

    Audit information is unnecessary; restore the file_set_helper methods in curation concerns

--- app/helpers/file_set_helper.rb
+++ app/helpers/file_set_helper.rb
@@ -1,15 +1,11 @@
-module CurationConcerns::FileSetHelper
-  def parent_path(parent)
-    if parent.is_a?(Collection)
-      main_app.collection_path(parent)
-    else
-      polymorphic_path([main_app, parent])
-    end
-  end
+module FileSetHelper
+  include CurationConcerns::FileSetHelper
 
-  # REVIEW: Since this media display could theoretically work for
-  #         any object that inplements to_s and the Mime Type methos (image? audio? ...),
-  #         Should this really be in file_set or could it be in it's own helper class like media_helper?
+  def present_terms(presenter, terms = :all, &block)
+    terms = presenter.terms if terms == :all
+    Sufia::PresenterRenderer.new(presenter, self).fields(terms, &block)
+  end
+  
   def media_display_full(presenter, locals = {})
     render media_display_partial_full(presenter),
            locals.merge(file_set: presenter)
