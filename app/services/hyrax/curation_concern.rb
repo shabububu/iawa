4f98e677db079d25f0133deefa2eb37033f54933
Lee Hunter <whunter@vt.edu>
2017-07-31 19:44:12 +0000

    temp fix for actor_factory bug

---
+++ app/services/hyrax/curation_concern.rb
@@ -0,0 +1,17 @@
+module Hyrax
+  class CurationConcern
+    # This attribute is set by Hyrax::Engine
+    class_attribute :actor_factory
+
+    # A consumer of this method can inject a different factory
+    # into this class in order to change the behavior of this method.
+    # @param [ActiveFedora::Base] curation_concern a work to be updated
+    # @param [Ability] current_ability the permission object for depositing this
+    #   work.
+    # @return [#create, #update] an actor that can create and update the work
+    def self.actor(curation_concern, current_ability)
+      Hyrax::CurationConcern.actor_factory = Hyrax::ActorFactory
+      actor_factory.build(curation_concern, current_ability)
+    end
+  end
+end
