0388cb6f8f36073015bcde017f57aa12aa03263e
Tingting Jiang <tvj@vt.edu>
2017-10-11 16:41:04 +0000

    Not default active workflow config is necessary

--- config/hyrax.rb
+++
@@ -1,6 +0,0 @@
-Hyrax.config do |config|
-  # When an admin set is created, we need to activate a workflow.
-  # The :default_active_workflow_name is the name of the workflow we will activate.
-  # @see Hyrax::Configuration for additional details and defaults.
-  config.default_active_workflow_name = 'default'
-end
