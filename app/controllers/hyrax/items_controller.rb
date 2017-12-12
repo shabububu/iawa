2bfed372220c980d70a36ed38ea53425d49d1de1
Tingting Jiang <tvj@vt.edu>
2017-12-12 20:25:49 +0000

    LIBTD-1035: Generate IIIF manifests so that images can be viewed by IIIF presentation API-enabled viewers

--- app/controllers/hyrax/items_controller.rb
+++ app/controllers/hyrax/items_controller.rb
@@ -9,10 +9,26 @@ module Hyrax
     self.curation_concern_type = ::Item
     self.show_presenter = ::ItemPresenter
 
+    skip_load_and_authorize_resource only: :manifest
+
     def show
       super
       item = Item.find(params[:id])
       @item_set = item.item_set
+      @images = item.file_sets(&:image?)
     end
+
+    def manifest
+      headers['Access-Control-Allow-Origin'] = '*'
+      respond_to do |format|
+        format.json { render json: manifest_builder.to_h }
+      end
+    end
+
+    private
+
+      def manifest_builder
+        IIIFManifest::ManifestFactory.new(presenter)
+      end
   end
 end
--- app/presenters/item_presenter.rb
+++ app/presenters/item_presenter.rb
@@ -1,4 +1,16 @@
 class ItemPresenter < Hyrax::WorkShowPresenter
   delegate :tags, :date, :coverage, :location, :medium, :bibliographic_citation,
            :rights_holder, :format, :contributor, to: :solr_document
+
+  Hyrax::MemberPresenterFactory.file_presenter_class = FileSetPresenter
+
+  def manifest_url
+    manifest_helper.polymorphic_url([:manifest, self])
+  end
+
+  private
+
+    def manifest_helper
+      @manifest_helper ||= ManifestHelper.new(request.base_url)
+    end
 end
